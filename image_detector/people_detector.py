602de72ab6006cf5fecf2eaa66b613a69bee364f
Nathan Lam <nathancy@hawaii.edu>
2018-10-11 12:04:00 -1000

    Malisiewicz Non-Maximum Suppression

--- image_detector/people_detector.py
+++ image_detector/people_detector.py
@@ -1,7 +1,6 @@
 from __future__ import print_function
 import cv2
 import numpy as np
-from imutils.object_detection import non_max_suppression
 from imutils import paths
 import argparse
 import imutils
@@ -13,6 +12,56 @@ imageDetector.initializeStream()
 
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+# Malisiewicz method for Non-Maximum suppression
+def non_max_suppression_fast(boxes, overlapThresh=0.35):
+    # If there are no boxes, return empty list
+    if len(boxes) == 0:
+        return []
+    # If the bounding boxes are integers, convert them to float
+    if boxes.dtype.kind == 'i':
+        boxes = boxes.astype('float')
+    # Initialize the list of picked indexes
+    pick = []
+
+    # Grab the coordinates of the bounding boxes
+    x1 = boxes[:,0]
+    y1 = boxes[:,1]
+    x2 = boxes[:,2]
+    y2 = boxes[:,3]
+
+    # Compute the area of the bounding boxes and sort by 
+    # the bottom-right y-coordinate of the bounding box
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
+    idxs = np.argsort(y2)
+
+    # Keep looping while some indexes still remain in the list
+    while len(idxs) > 0:
+        # Grab the last index in the list and add the index value to
+        # the list of picked indexes
+        last = len(idxs) -1
+        i = idxs[last]
+        pick.append(i)
+
+        # Find the largest (x,y) coordinates for the start of the bounding box 
+        # and the smallest (x,y) coordinates for the end of the bounding box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+
+        # Compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+
+        # Compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+
+        # Delete all indexes from the list
+        idxs = np.delete(idxs, np.concatenate(([last], np.where(overlap > overlapThresh)[0])))
+
+    # Return only the bounding boxes that were picked using the integer data type
+    return boxes[pick].astype('int')
 
 while(imageDetector.isOpened()):
     # Load IP camera and resize frame to reduce detection time and improve detection accuracy
@@ -27,19 +76,10 @@ while(imageDetector.isOpened()):
     # large overlap threshold to try to maintain overlapping
     # boxes that are still people
     rects = np.array([[x,y,x+w, y+h] for (x,y,w,h) in rects])
-    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+    pick = non_max_suppression_fast(rects, overlapThresh=0.35)
 
     # Draw the final bounding boxes
     for(xA, yA, xB, yB) in pick:
         cv2.rectangle(image, (xA, yA), (xB, yB), (0,255,0), 2)
     imageDetector.showFrame(image)
-
-    
-
-
-    
-
-
-
-
 
